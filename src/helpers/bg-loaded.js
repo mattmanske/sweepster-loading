e93d637e972effa5216be65d7ceea6c761948c8e
mattmanske <matt@manske.me>
2017-07-07 13:57:44 -0500

    Move to CSS Animations

--- src/helpers/bg-loaded.js
+++ src/helpers/bg-loaded.js
@@ -1,23 +1,23 @@
 //-----------  Helper -----------//
-//
-// function getBgUrl(){
-//   const $elem = document.getElementById('sweep-elem')
-//   let bg = ''
-//
-//   if ($elem.currentStyle) // IE
-//     bg = $elem.currentStyle.backgroundImage;
-//   else if (document.defaultView && document.defaultView.getComputedStyle) // Firefox
-//     bg = document.defaultView.getComputedStyle($elem, '').backgroundImage
-//   else // try and get inline style
-//     bg = $elem.style.backgroundImage
-//
-//   return bg.replace(/url\(['"]?(.*?)['"]?\)/i, '$1')
-// }
+
+function getBgUrl(){
+  const $elem = document.getElementById('sweep-elem')
+  let bg = ''
+
+  if ($elem.currentStyle) // IE
+    bg = $elem.currentStyle.backgroundImage;
+  else if (document.defaultView && document.defaultView.getComputedStyle) // Firefox
+    bg = document.defaultView.getComputedStyle($elem, '').backgroundImage
+  else // try and get inline style
+    bg = $elem.style.backgroundImage
+
+  return bg.replace(/url\(['"]?(.*?)['"]?\)/i, '$1')
+}
 
 //-----------  Detect Background Image Load  -----------//
 
-export default function detectBgLoad(fileSrc, callback){
+export default function detectBgLoad(callback){
   const image = document.createElement('img')
-  image.src    = fileSrc
+  image.src    = getBgUrl()
   image.onload = callback
 }
--- src/loader.js
+++ src/loader.js
@@ -1,47 +1,68 @@
+import './styles.css'
+
 import detectBgLoad from 'helpers/bg-loaded'
-
-import animateElem  from 'helpers/animate-elem'
-import animateLogo  from 'helpers/animate-logo'
-import animateLoad  from 'helpers/animate-load'
-import animateStat  from 'helpers/animate-stat'
-
 import updateState  from 'helpers/update-state'
 
 //-----------  Load Watcher  -----------//
 
-detectBgLoad('greenBg.jpg', () => {
+detectBgLoad(() => {
 
   //-----------  Definitions  -----------//
 
   const $elem = document.getElementById('sweep-elem')
+  const $logo = document.getElementById('sweep-logo')
+  const $load = document.getElementById('sweep-load')
+  const $stat = document.getElementById('sweep-stat')
 
-  const start = document.createEvent('CustomEvent')
-  start.initCustomEvent('startLoad', true, true)
+  //-----------  Event Dispatchers  -----------//
 
-  const ended = document.createEvent('CustomEvent')
-  ended.initCustomEvent('endedLoad', true, true)
+  function dispatchStart(){
+    console.log('start');
+    $stat.removeEventListener('animationend', dispatchStart, false)
 
-  //-----------  Setup Event Listeners  -----------//
-
-  $elem.addEventListener('updateState', updateState)
-
-  //-----------  Initial Animations  -----------//
-
-  animateElem('show', () => {
-    animateLogo('show', () => console.log('logo'))
-    animateLoad('show', () => console.log('load'))
-    animateStat('show', () => console.log('stat'))
-
+    const start = document.createEvent('CustomEvent')
+    start.initCustomEvent('startLoad', true, true)
     document.dispatchEvent(start)
+  }
 
-    $elem.addEventListener('loadComplete', () => {
-      animateLogo('hide', () => console.log('logo'))
-      animateLoad('hide', () => console.log('load'))
-      animateStat('hide', () => console.log('stat'))
-      animateElem('hide', () => {
-        document.dispatchEvent(ended)
-        $elem.remove()
-      })
-    })
-  })
+  function dispatchEnded(){
+    console.log('end');
+    $elem.removeEventListener('animationend', dispatchEnded, false)
+
+    const ended = document.createEvent('CustomEvent')
+    ended.initCustomEvent('endedLoad', true, true)
+    document.dispatchEvent(ended)
+
+    $elem.remove()
+    $load.remove()
+  }
+
+  function loadIn(){
+    $elem.removeEventListener('animationend', loadIn, false)
+
+    $logo.classList.add('in')
+    $stat.classList.add('in')
+  }
+
+  function loadOut(){
+    $elem.addEventListener('animationend', dispatchEnded, false)
+
+    $logo.classList.add('out')
+    $stat.classList.add('out')
+    $elem.classList.add('out')
+    $load.classList.add('out')
+  }
+
+  //-----------  Animation Event Listeners  -----------//
+
+  $elem.addEventListener('updateState', updateState, false)
+  $elem.addEventListener('loadComplete', loadOut, false)
+
+  $elem.addEventListener('animationend', loadIn, false)
+  $stat.addEventListener('animationend', dispatchStart, false)
+
+  //-----------  In Animations  -----------//
+
+  $elem.classList.add('in')
+  $load.classList.add('in')
 })
